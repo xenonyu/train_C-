db032b4bad1b234adbac4230413d2fcfc01fb241
xenonyu <yumingxie46@gmail.com>
2021-02-28 00:45:11 +0800

    formated

--- test.cpp
+++ test.cpp
@@ -3,20 +3,19 @@
 #include <string>
 
 using namespace std;
-void Log(const char* message);
+void Log(const char *message);
 
 template <typename T>
-T sum(T a, T b){
-    return a+b;
+T sum(T a, T b)
+{
+    return a + b;
 }
 
-int main(){
-    int a = 3, b = 4, result;
-    long c = 3;
-    char d = 65;
-    cout << "char version of 4:  " << d << endl;
-    cout << "size of long: " << sizeof(long) << endl;
-    result = sum<int> (a, b);
+int main()
+{
+    bool a = true;
+    cout << a << endl;
+    int result = sum<int>(2, 3);
     cout << "result = " << result << endl;
     Log("hello world.");
 }
